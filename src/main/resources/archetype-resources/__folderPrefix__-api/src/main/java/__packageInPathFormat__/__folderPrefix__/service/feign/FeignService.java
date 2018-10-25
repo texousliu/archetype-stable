75c6375051f2e7ead881a28eed61408a64a9655f
liuxiaohua <504579863@qq.com>
2018-10-25 09:34:50 +0800

    修改扫描包

--- src/main/resources/archetype-resources/__folderPrefix__-api/src/main/java/__packageInPathFormat__/__folderPrefix__/service/feign/FeignService.java
+++ src/main/resources/archetype-resources/__folderPrefix__-api/src/main/java/__packageInPathFormat__/__folderPrefix__/service/feign/FeignService.java
@@ -4,6 +4,6 @@ package ${package}.${folderPrefix}.service.feign;
 public interface FeignService {
 
     String NAME = ${symbol_pound}${artifactId}${symbol_pound};
-    String PACKAGE = ${symbol_pound}${package}.${folderPrefix}.service.feign${symbol_pound};
+    String PACKAGE = ${symbol_pound}${package}.${folderPrefix}.service${symbol_pound};
 
 }
